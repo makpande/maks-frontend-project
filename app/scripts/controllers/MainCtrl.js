6e3800632d88960ba0de3846df20acba106a7167
makpande <mak_pande@yahoo.com>
2016-03-24 21:57:48 -0400

    start and reset for bloc time

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -1,11 +1,51 @@
-// (function () {
-//     function MainCtrl($scope, $interval) {
-//
-//
-//       }
-//
-//
-//     angular
-//         .module('maksBlocTime')
-//         .controller('MainCtrl', ['$interval', '$scope', MainCtrl]);
-// })();
+(function () {
+
+    function MainCtrl($scope, $interval) {
+      console.log("main");
+      var myDataRef = new Firebase('https://maksbloctime.firebaseio.com/');
+      $scope.time = (60*25);
+
+      $scope.taskName = 'Start';
+
+      var stop;
+
+      $scope.countdown = function() {
+
+        if ( angular.isDefined(stop) ) return;
+
+        stop = $interval(function() {
+          $scope.time -= 1;
+        }, 1000);
+
+        $scope.taskName = 'Reset';
+      };
+
+      $scope.stopTime = function() {
+        if (angular.isDefined(stop)) {
+          $interval.cancel(stop);
+          stop = undefined;
+        }
+        $scope.taskName = 'Start';
+      };
+
+      $scope.reset = function() {
+        $scope.stopTime();
+        $scope.time = (60*25);
+      };
+
+      $scope.startReset = function() {
+        if ($scope.taskName == 'Start' ){
+          $scope.countdown();
+        }
+        else{
+          $scope.reset();
+        }
+      };
+
+      }
+
+
+    angular
+        .module('maksBlocTime')
+        .controller('MainCtrl', ['$scope', '$interval',  MainCtrl]);
+})();
--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,70 +1,22 @@
 
-// (function () {
-//     function config($stateProvider, $locationProvider) {
-//         $locationProvider
-//             .html5Mode({
-//                 enabled: true,
-//                 requireBase: false
-//             });
-//
-//         $stateProvider
-//             .state('main', {
-//                 url: '/',
-//                 controller: 'MainCtrl as main',
-//                 templateUrl: '/templates/main.html'
-//             });
-//     }
-//
-//     angular
-//         .module('maksBlocTime', ['ui.router', 'firebase'])
-//         .config(config);
-//
-// })();
+(function () {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
 
-
-var app = angular.module("maksBlocTime", ['firebase', 'ui.router']);
-var myDataRef = new Firebase('https://maksbloctime.firebaseio.com/');
-
-app.controller('MainCtrl', ['$scope', '$interval', function($scope, $interval) {
-
-  $scope.time = (60*25);
-
-  $scope.taskName = 'Start';
-
-  var stop;
-
-  $scope.countdown = function() {
-
-    if ( angular.isDefined(stop) ) return;
-
-    stop = $interval(function() {
-      $scope.time -= 1;
-    }, 1000);
-
-    $scope.taskName = 'Reset';
-  };
-
-  $scope.stopTime = function() {
-    if (angular.isDefined(stop)) {
-      $interval.cancel(stop);
-      stop = undefined;
+        $stateProvider
+            .state('main', {
+                url: '/',
+                controller: 'MainCtrl as main',
+                templateUrl: '/templates/main.html'
+            });
     }
-    $scope.taskName = 'Start';
-  };
 
-  $scope.reset = function() {
-    $scope.stopTime();
-    $scope.time = (60*25);
-  };
+    angular
+        .module('maksBlocTime', ['ui.router', 'firebase'])
+        .config(config);
 
-  $scope.startReset = function() {
-    if ($scope.taskName == 'Start' ){
-      $scope.countdown();
-    }
-    else{
-      $scope.reset();
-    }
-  };
-
-
-}]);
+})();
